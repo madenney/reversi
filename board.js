f469b5ef388b9d25120a8df3158e684f0bb07fef
Matt <matt.a.denney@gmail.com>
2017-06-19 23:28:09 -0700

    Added some basic oop functions

--- board.js
+++ board.js
@@ -1,3 +1,127 @@
 /**
  * Created by Matt on 6/19/2017.
+ *
+ *
+ * To make this easier, Open Adjacent Tiles will be called OATs
  */
+
+function Board(x,y) {
+
+    var rows = x;
+    var cols = y;
+    var tiles = [];
+
+    var initialize = function() {
+        console.log("Initializing Board Object");
+        var maxTileContainerWidth = 150;
+
+        // Get some values and do some math to find width of tile container
+        var boardWidth = $("#board-container").css("width").slice(0, -2);
+        boardWidth = boardWidth - $("#board-container").css("border-width").slice(0, 2);
+        var boardHeight = $("#board-container").css("height").slice(0, -2);
+        boardHeight = boardHeight - $("#board-container").css("border-width").slice(0, 2);
+        var potentialTileContainerWidth = boardWidth / cols;
+        var potentialTileContainerHeight = boardHeight / rows;
+        if (potentialTileContainerHeight < potentialTileContainerWidth) {
+            var tileContainerWidth = potentialTileContainerHeight;
+        } else {
+            var tileContainerWidth = potentialTileContainerWidth;
+        }
+        if (tileContainerWidth > maxTileContainerWidth) {
+            tileContainerWidth = maxTileContainerWidth;
+        }
+        //tileContainerWidth = Math.floor(tileContainerWidth);
+        console.log("tileContainerWidth: " + tileContainerWidth);
+        // Create board
+        var board = $("<div>").attr("id", "board");
+        board.css({"width": tileContainerWidth * cols, "height": tileContainerWidth * rows});
+
+        // Create tile containers
+        for (var i = 0; i < rows; i++) {
+            var row = [];
+            for (var j = 0; j < cols; j++) {
+                // Create tile containers and add them to the board
+                var tileContainer = $("<div>").addClass("tile-container");
+                tileContainer.css({"width": tileContainerWidth, "height": tileContainerWidth});
+                var tileElement = $("<div>").addClass("tile");
+                var tileID = "" + i + "-" + j;
+                tileElement.attr("id", tileID);
+                tileContainer.text(tileID);
+                tileContainer.append(tileElement);
+                board.append(tileContainer);
+
+                // Create tile object and add it to the tiles array
+                var tile = new Tile(i, j);
+                row.push(tile);
+            }
+            tiles.push(row);
+        }
+        $("#board-container").append(board);
+    };
+
+    this.setup = function() {
+        tiles[3][3].setColor("white");
+        tiles[3][4].setColor("black");
+        tiles[4][3].setColor("black");
+        tiles[4][4].setColor("white");
+    };
+
+    var getOccupiedTiles = function() {
+        var arr = [];
+        for(var i = 0; i < tiles.length; i++) {
+            for (var j = 0; j < tiles[0].length; j++) {
+                if(tiles[i][j].getColor() !== "none") {
+                    arr.push(tiles[i][j]);
+                }
+            }
+        }
+        return arr;
+    };
+
+    var getAdjacentTiles = function(tile) {
+        var arr = [];
+        var coords = tile.getCoords();
+        for(var i = -1; i < 2; i++) {
+            for(var j = -1; j < 2; j++) {
+                if(i == 0 && j == 0) {continue;} // Skip self
+                arr.push(tiles[Number(coords[0]) + i][Number(coords[1]) + j]);
+            }
+        }
+        return arr;
+    };
+
+    this.getOATs = function() {
+        var occupied = getOccupiedTiles();
+        var OATs = [];
+        for(var i = 0; i < occupied.length; i++) {
+            var adjacentTiles = getAdjacentTiles(occupied[i]);
+            for (var j = 0; j < adjacentTiles.length; j++){
+                if(adjacentTiles[j].getColor() === "none"){
+                    addWithoutRepeats(adjacentTiles[j], OATs);
+                }
+            }
+        }
+        return OATs;
+    };
+
+
+    this.logTiles = function(){
+        console.log("Logging Tiles: ");
+        for(var i = 0; i < tiles.length; i++) {
+            for(var j = 0; j < tiles[0].length; j++) {
+                tiles[i][j].log();
+            }
+        }
+    };
+
+    var addWithoutRepeats = function(newTile, tileArray) {
+        for(var i = 0; i < tileArray.length; i++) {
+            if(tileArray[i] === newTile) {
+                return;
+            }
+        }
+        tileArray.push(newTile);
+    };
+
+    initialize();
+}
--- game.js
+++ game.js
@@ -1,3 +1,23 @@
 /**
  * Created by Matt on 6/19/2017.
  */
+
+function Game() {
+
+    var board;
+    var whiteScore = 0;
+    var blackScore = 0;
+    var turn = "black";
+
+    var initialize = function() {
+        board = new Board(8,8);
+        board.setup();
+
+        x = board.getOATs();
+        for(var i = 0; i < x.length; i++) {
+            console.log(x[i].getId());
+        }
+
+    };
+    initialize();
+}
